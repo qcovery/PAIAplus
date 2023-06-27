62a23d7d80900ecdf9a0d219774493695fcaac1c
Oliver Stöhr <stoehr@effective-webwork.de>
2023-06-27 16:50:12 +0200

    Remove custom Connection class

--- config/module.config.php
+++ config/module.config.php
@@ -6,15 +6,12 @@ $config = [
         'allow_override' => true,
         'factories' => [
             'PAIAplus\Auth\ILSAuthenticator' => 'PAIAplus\Auth\ILSAuthenticatorFactory',
-            'PAIAplus\ILS\Connection' => 'PAIAplus\ILS\ConnectionFactory',
             'PAIAplus\ILS\Driver\PAIA' => 'PAIAplus\ILS\Driver\PAIAFactory',
         ],
         'aliases' => [
             'VuFind\Auth\ILSAuthenticator' => 'PAIAplus\Auth\ILSAuthenticator',
             'VuFind\ILSAuthenticator' => 'PAIAplus\Auth\ILSAuthenticator',
             'paia' => 'PAIAplus\ILS\Driver\PAIA',
-            'VuFind\ILS\Connection' => 'PAIAplus\ILS\Connection',
-            'VuFind\ILSConnection' => 'PAIAplus\ILS\Connection',
         ],
     ],
     'controllers' => [
